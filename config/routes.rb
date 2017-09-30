ead02dde53392be23281dee24ff8622bfd954777
better-tommorrow <SEOL@SEOLxDREAM.local>
2017-09-30 19:20:39 +0900

    Update: convert database of RoR from SqlLite to mysql

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: "feeds#index"
 
   resources :feeds, only: %w(index show)
+  resources :categories, only: %w(index)
+  resources :home, only: %w(index)
+  resources :articles, only: %w(index)
+  resources :resume, only: %w(index)
 end
