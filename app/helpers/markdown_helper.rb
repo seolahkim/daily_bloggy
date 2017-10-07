8861e20c486069cff331fa2407cdc34f8531ecfb
better-tommorrow <SEOL@SEOLxDREAM.local>
2017-10-07 20:18:44 +0900

    Feat(Markdown): Add MarkdownHelper to format articles

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,25 @@
+module MarkdownHelper
+  REDCARPET_OPTIONS = {
+    autolink: true,
+    space_after_headers: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    tables: true,
+    hard_wrap: true,
+    xhtml: true,
+    lax_html_blocks: true,
+    strikethrough: true
+  }.freeze
+
+  def markdown_to_html(md_code)
+    html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(html_render, REDCARPET_OPTIONS)
+    markdown.render(md_code)
+  end
+
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language || 'md').div
+    end
+  end
+end
