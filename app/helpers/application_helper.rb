68781f41ddcf66827db47e3ec20f3f38d7b02c7a
better-tommorrow <SEOL@SEOLxDREAM.local>
2017-10-07 20:21:25 +0900

    Beautify application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-  def categories_tab(_categories)
-    html = ''
-    categories = _categories.where(parent_id: nil)
-    categories.each do |c|
-      sub_category_tags = []
-      sub_category_tags = c.sub_categories.map{|s| content_tag(:div, "- " + s.parent, class: "sub-category") } if c.sub_categories.present?
-      category_tag = content_tag(:div, c.parent, class: "category")
-      html += content_tag(:div, category_tag + sub_category_tags.join('').html_safe)
-    end
-    
-    html.html_safe
-  end
+  include MarkdownHelper
+  include FooterHelper
 end
