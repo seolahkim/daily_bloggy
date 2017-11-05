9f1d4c84a4565f0913bcc28277902b659975c890
better-tommorrow <SEOL@SEOLxDREAM.local>
2017-11-05 14:35:58 +0900

    Turn div tag of sub category to link

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -7,11 +7,12 @@ module FooterHelper
       if c.sub_categories.present?
         sub_category_tags = 
         c.sub_categories.map do |s| 
-          
           related_articles = Article.related_category(s.id)
-          content_tag(:div, "- #{s.parent} (#{related_articles.count})", class: "sub-category")
+          link_tag = link_to("- #{s.parent} (#{related_articles.count})", category_path(c.id))
+          content_tag(:div, link_tag, class: "sub-category")
         end
       end
+      
       
       category_tag = content_tag(:div, c.parent, class: "category")
       html += content_tag(:div, category_tag + sub_category_tags.join('').html_safe)
