58644b36dd78b534c41c61f525213c80a5908068
Kim <SORA@SEOL.local>
2017-11-21 21:39:17 +0900

    Working: Update stylesheets (including highlight text color)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+  include MarkdownHelper
+
   scope :related_category, ->(id) { where(category_id: id) }
   scope :recent,           -> { order(updated_at: :desc) }
+
+  def rendered_content
+    markdown_to_html self.content
+  end
 end
